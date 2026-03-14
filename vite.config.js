fdf024472f2ac0d7a3fad7e87584cf36448130ed
Chukwuemeka David <chukkydave@gmail.com>
2026-03-14 14:34:03 +0100

    fix vite issue

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,26 @@ import react from '@vitejs/plugin-react'
 import fs from 'fs'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    https: {
-      key: fs.readFileSync('localhost-key.pem'),
-      cert: fs.readFileSync('localhost.pem'),
-    },
-    port: 5173
+export default defineConfig(({ mode }) => {
+  const httpsConfig = mode !== 'production'
+    ? (() => {
+      try {
+        return {
+          key: fs.readFileSync('localhost-key.pem'),
+          cert: fs.readFileSync('localhost.pem'),
+        }
+      } catch {
+        return false
+      }
+    })()
+    : false
+
+  return {
+    plugins: [react()],
+    server: {
+      https: httpsConfig,
+      port: 5173
+    }
   }
 })
 
